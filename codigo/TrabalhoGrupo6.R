9d91251e6731e6119635790f04b6c98f3838f2f4
winy <winy2000@gmail.com>
2018-11-13 21:51:05 -0200

    Script inicial

---
+++ codigo/TrabalhoGrupo6.R
@@ -0,0 +1,10 @@
+install.packages("rmarkdown")
+install.packages("tinytex")
+library(rmarkdown)
+library(tinytex)
+install_tinytex(force = TRUE, dir = "auto", repository = "ctan", extra_packages = NULL)
+tinytex_root()
+
+use_tinytex(from = "C:\\projetos\\project-rmarkdown\\auto")
+
+> tinytex::tinytex_root()
